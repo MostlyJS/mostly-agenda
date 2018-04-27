2e937a9cc338a33f7bb3fd1aec50ce5b5ad9a96d
forafish <fora.luo@gmail.com>
2018-04-27 19:15:09 +0800

    remove finished jobs for a time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Agenda from 'agenda';
 import fp from 'mostly-func';
+import humanInterval from 'human-interval';
 
 const defaultOptions = {
   db: {
@@ -9,13 +10,21 @@ const defaultOptions = {
         auto_reconnect: true
       }
     }
-  }
+  },
+  cleanAfter: '1 week'
 };
 
 export default function (options) {
   options = fp.assign({}, options, defaultOptions);
   const agenda = new Agenda(options);
   agenda.on('ready', agenda.start);
+  agenda.on('complete', job => {
+    // remove finished jobs for a time
+    const cleanDate = new Date(Date.now() - humanInterval(options.cleanAfter));
+    agenda.cancel({ nextRunAt: null, lastFinishedAt: { $lte: cleanDate } }, (err) => {
+      if (err) console.error('Failed to clean finished agenda jobs:', err);
+    });
+  });
 
   return function () {
     const app = this;
